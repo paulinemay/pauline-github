9706e509e526982f7a732fedb4e2710df93f8966
paulinemay <paulinebacal5@gmail.com>
2019-03-23 12:44:21 +0800

    first commit

---
+++ application/views/home.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title> Lab activity Git and Git HUB </title>
+<link href="assets/css/mystyle.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<h1> THIS IS MY LAB ACTIVITY - Bacal </h1>
+	<script src="<?php echo "assets/js/myscript.js"; ?>" ></script>
+</body>
+</html>
